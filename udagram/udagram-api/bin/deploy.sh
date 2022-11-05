d599a6bb1030efd83c9604a09175fda237eca7f2
Yasmin-Hisham <yasminhisham63@gmail.com>
2022-11-05 19:21:19 +0200

    Modify deploy file

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -2,7 +2,6 @@ set -x
 set -e
 
 eb init  udagram-api  --platform node.js --region us-east-1
-eb create --sample udagram-api-env
 eb use udagram-api-env
 
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
